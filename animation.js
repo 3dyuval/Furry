3b4a27cf7b970e0af30402eb33f0830db78414a6
3dyuval <3dyuval@gmail.com>
2021-09-11 16:47:26 +0300

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(TextPlugin, ScrollToPlugin, ScrollTrigger);
+
+// const headings = gsap.utils.toArray("h1");
+
+// headings.forEach((heading) => {
+//   gsap.from(heading, {
+//     scrollTrigger: {
+//       trigger: heading,
+//       start: "bottom bottom",
+//     },
+//     duration: 1,
+//     y: -20,
+//     opacity: 0,
+//   });
+// });
